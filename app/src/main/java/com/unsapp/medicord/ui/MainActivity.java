8109f5db556018609e1086a93a6d75332c31ef16
EduardoKstillo <eddy.castillo.dev@gmail.com>
2024-02-06 01:17:12 -0500

    se corrigio la visibilidad de navegation en login and register fragment

--- app/src/main/java/com/unsapp/medicord/ui/MainActivity.java
+++ app/src/main/java/com/unsapp/medicord/ui/MainActivity.java
@@ -1,7 +1,9 @@
 package com.unsapp.medicord.ui;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.unsapp.medicord.R;
 import com.unsapp.medicord.data.models.User;
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements
         RegisterFragment.OnRegisterSuccessListener{
 
      ActivityMainBinding binding;
+     private BottomNavigationView bottomNavigationView;
 
      private UserViewModel userViewModel;
 
@@ -39,6 +43,8 @@ public class MainActivity extends AppCompatActivity implements
 
         userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
 
+        bottomNavigationView = binding.bottomNavigation;
+
         // LoginFragment instance
         LoginFragment loginFragment = new LoginFragment();
 
@@ -48,6 +54,8 @@ public class MainActivity extends AppCompatActivity implements
         loginFragment.setOnLoginSuccessListener(this);
         // load fragment -> LoginFragment
         loadFragment(loginFragment);
+        hideBottomNavigationView();
+
 
         // navigation
         binding.bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
@@ -91,124 +99,29 @@ public class MainActivity extends AppCompatActivity implements
         RegisterFragment registerFragment = new RegisterFragment();
         registerFragment.setOnRegisterSuccessListener(this);
         loadFragment(registerFragment);
+        hideBottomNavigationView();
     }
 
     @Override
     public void onLoginSuccess() {
         // when login is successful
         loadFragment(new HomeFragment());
+        showBottomNavigationView();
     }
 
     @Override
     public void onRegisterSuccess() {
         // when user registration is successful
         loadFragment(new HomeFragment());
+        showBottomNavigationView();
+    }
+
+    private void hideBottomNavigationView() {
+        bottomNavigationView.setVisibility(View.GONE);
+    }
+
+    private void showBottomNavigationView() {
+        bottomNavigationView.setVisibility(View.VISIBLE);
     }
 }
 
-
-
-/*
-public class MainActivity extends AppCompatActivity {
-
-    private EditText etName, etEmail, etPassword;
-
-    private AppDatabase appDatabase;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        etName = findViewById(R.id.etName);
-        etEmail = findViewById(R.id.etEmail);
-        etPassword = findViewById(R.id.etPassword);
-
-        appDatabase = Room.databaseBuilder(getApplicationContext(),
-                AppDatabase.class, "medicord_db").allowMainThreadQueries().build();
-
-
-        //appDatabase = AppDatabase.getInstance(this);
-
-
-        // button to register
-        Button btnSingUp = findViewById(R.id.btnSingUp);
-        btnSingUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // function for register to user
-                registerUser();
-            }
-        });
-
-        Button btnLogin = findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // function to login
-                loginUser();
-            }
-        });
-    }
-
-    // function for register to user
-    private void registerUser(){
-
-        // get attributes and convert them to string
-        String name = etName.getText().toString();
-        String email = etEmail.getText().toString();
-        String password = etPassword.getText().toString();
-
-        // sim os campos tão empty
-        if (name.isEmpty() || email.isEmpty() || password.isEmpty()){
-            Toast.makeText(this, "Complete all fields", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        // check if the user already exists
-        if (userAlready(email)){
-            Toast.makeText(this, "The user is already registered", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        // register new user
-        User user = new User();
-        user.username = name;
-        user.email = email;
-        user.password = password;
-
-        UserDao userDao = appDatabase.userDao();
-        userDao.insertUser(user);
-
-        Toast.makeText(this,"Successfully registered user", Toast.LENGTH_SHORT).show();
-
-        // more functionalities
-
-    }
-
-    private boolean userAlready(String email){
-        return appDatabase.userDao().findByEmail(email);
-    }
-
-    private void loginUser(){
-        String email = etEmail.getText().toString();
-        String password = etPassword.getText().toString();
-
-        // sim os campos tão empty
-        if (email.isEmpty() || password.isEmpty()){
-            Toast.makeText(this, "Complete all fields", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        User user = appDatabase.userDao().findByEmailAndPassword(email, password);
-
-        if (user == null){
-            Toast.makeText(this, "Incorrect credentials", Toast.LENGTH_SHORT).show();
-        }
-
-        Toast.makeText(this, "Successful login", Toast.LENGTH_SHORT).show();
-        // more functionalities
-
-    }
-}
-*/
